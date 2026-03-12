044eeed30a09e96a6779a7e1310c12525c541ac6
Hazem Gamal <hazemgamal223311@gmail.com>
2026-03-12 09:06:58 +0200

    add model

---
+++ src/model.py
@@ -0,0 +1,21 @@
+from torchvision import models
+import torch.nn as nn
+
+
+class PlantDiseaseClassifier(nn.Module):
+    def __init__(self, num_classes=15, freeze_backbone=True):
+        super().__init__()
+
+        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+
+        if freeze_backbone:
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+        in_features = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
+            nn.Dropout(p=0.5), nn.Linear(in_features, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
