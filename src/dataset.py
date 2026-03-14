ab7d0a8fc11fc9616d08c4a6319271f8f2024b4e
Hazem Gamal <hazemgamal223311@gmail.com>
2026-03-14 08:25:03 +0200

    increasse number of workers

--- src/dataset.py
+++ src/dataset.py
@@ -71,20 +71,20 @@ def prepare_datasets(config):
         train_dataset,
         batch_size=config["train"]["batch_size"],
         shuffle=True,
-        num_workers=2
+        num_workers=4
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=config["train"]["batch_size"],
         shuffle=False,
-        num_workers=2
+        num_workers=4
 
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=config["train"]["batch_size"],
         shuffle=False,
-        num_workers=2
+        num_workers=4
     )
 
     return train_loader, val_loader, test_loader
